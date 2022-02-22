650d3f1e4fa612f758111fb02703404adc80013b
Archana K B <84127763+arch0nline@users.noreply.github.com>
2022-02-22 20:44:10 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -16,8 +16,14 @@ function generatePassword(){
 function populatePasswords() {
     
     passwordLength = Number(document.getElementById("passwordLength").value);
-    document.getElementById("outputBox1").textContent = generatePassword();
-    document.getElementById("outputBox2").textContent = generatePassword();
-    document.getElementById("outputBox3").textContent = generatePassword();
-    document.getElementById("outputBox4").textContent = generatePassword();
+    if (passwordLength >= 6 && passwordLength <=18) {
+        document.getElementById("outputBox1").textContent = generatePassword();
+        document.getElementById("outputBox2").textContent = generatePassword();
+        document.getElementById("outputBox3").textContent = generatePassword();
+        document.getElementById("outputBox4").textContent = generatePassword();
+    }
+    else {
+        window.alert("Password length should be between 6 and 18! ")
+    }
+    
 }
